908618c48ae61e587dd8a2497373adb6727b24d4
Darko Filipovski <darkofilipovski0@gmail.com>
2020-07-07 12:15:24 +0200

    Parking CIS init

---
+++ parking-cis/src/main/java/com/filipovski/parkingguidance/parkingcis/ParkingCisApplication.java
@@ -0,0 +1,13 @@
+package com.filipovski.parkingguidance.parkingcis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingCisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingCisApplication.class, args);
+	}
+
+}
---
+++ parking-cis/src/test/java/com/filipovski/parkingguidance/parkingcis/ParkingCisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipovski.parkingguidance.parkingcis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingCisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
